4cfe782b199a86c5b5ea07d942e8096fdd2e7b00
Matt Dean <matt.dean.001@gmail.com>
2016-11-22 13:34:05 +0000

    definitely wont work

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('http');
+var server = http.createServer(function(request, response) {});
+server.listen(1234, function() {
+    console.log((new Date()) + ' Server is listening on port 1234');
+});
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+var count = 0;
+var clients = {};
+wsServer.on('request', function(r){
+  var connection = r.accept('echo-protocol', r.origin);
+  // Specific id for this client & increment count
+var id = count++;
+// Store the connection method so we can loop through & contact all clients
+clients[id] = connection;
+console.log((new Date()) + ' Connection accepted [' + id + ']');
+
+});
+
 //get postgres code from heroku docs
 var pg = require('pg');
 pg.defaults.ssl = true;
