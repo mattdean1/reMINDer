50d71d2a755bc457eea024a5f7ee1265940e86ec
Jules Gribble <gribblej@me.com>
2016-11-29 11:37:05 +0000

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+'use strict';
 var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
 	httpsEnabled: false,
-	port: process.env.PORT || 80
+	port: process.env.PORT || 80,
 } );
 
 server.start();
